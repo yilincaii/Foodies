e70cd464db76e5a893c196d6471c8158da814f23
nayuanju <yic153@ucsd.edu>
2025-09-19 21:06:27 -0700

    initial set with Spring Initializr and add maven cersion dependency

---
+++ src/test/java/com/example/foodiesapi/FoodiesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodiesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
