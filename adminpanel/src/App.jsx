ae31456ce738539df662414c208c7d4577706a3d
nayuanju <yic153@ucsd.edu>
2025-09-21 12:09:23 -0700

    define sidebar state & routing in App.jsx (add imports, Menubar toggle, body class toggle) to resolve “sidebarVisible is not defined” crash

--- adminpanel/src/App.jsx
+++ adminpanel/src/App.jsx
@@ -1,53 +1,39 @@
-import React from 'react'
-import { Routes } from 'react-router-dom';
+import React, { use, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import AddFood from './pages/AddFood/AddFood';
+import ListFood from './pages/ListFood/ListFood';
+import Orders from './pages/Orders/Orders';
+import Sidebar from './components/Sidebar/Sidebar';
+import Menubar from './components/Menubar/Menubar';
+import { ToastContainer } from 'react-toastify';
 
 const App = () => {
+  const [sidebarVisible, setSidebarVisible] = useState(true);
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
+  }
   return (
-        <div className="d-flex" id="wrapper">
-            <div className="border-end bg-white" id="sidebar-wrapper">
-                <div className="sidebar-heading border-bottom bg-light">Start Bootstrap</div>
-                <div className="list-group list-group-flush">
-                    <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Dashboard</a>
-                    <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Shortcuts</a>
-                    <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Overview</a>
-                    <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Events</a>
-                    <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Profile</a>
-                    <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Status</a>
-                </div>
-            </div>
+    <div className="d-flex" id="wrapper">
+            
+            <Sidebar sidebarVisible={sidebarVisible}/>
+            
             <div id="page-content-wrapper">
-                <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-                    <div className="container-fluid">
-                        <button className="btn btn-primary" id="sidebarToggle">Toggle Menu</button>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
-                                <li className="nav-item active"><a className="nav-link" href="#!">Home</a></li>
-                                <li className="nav-item"><a className="nav-link" href="#!">Link</a></li>
-                                <li className="nav-item dropdown">
-                                    <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                                    <div className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                        <a className="dropdown-item" href="#!">Action</a>
-                                        <a className="dropdown-item" href="#!">Another action</a>
-                                        <div className="dropdown-divider"></div>
-                                        <a className="dropdown-item" href="#!">Something else here</a>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+                
+                <Menubar toggleSidebar={toggleSidebar} />
+                <ToastContainer />
+                
                 <div className="container-fluid">
                     <Routes>
-                      <Route path='/add' element={<AddFood/>}/>
-                      <Route path='/list' element={<ListFood/>}/>
-                      <Route path='/orders' element={<Orders/>}/>
-                      <Route path='/' element={<ListFood/>}/>
+                      <Route path='/add' element={<AddFood />} />
+                      <Route path='/list' element={<ListFood />} />
+                      <Route path='/orders' element={<Orders />} />
+                      <Route path='/' element={<ListFood />} />
                     </Routes>
                 </div>
             </div>
         </div>
-          )
+  )
 }
 
 export default App;
--- adminpanel/src/components/Menubar/Menubar.jsx
+++ adminpanel/src/components/Menubar/Menubar.jsx
@@ -2,8 +2,28 @@ import React from 'react';
 
 const Menubar = () => {
   return (
-    <div>Menubar</div>
-  )
+        <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+            <div className="container-fluid">
+                <button className="btn btn-primary" id="sidebarToggle">Toggle Menu</button>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
+                        <li className="nav-item active"><a className="nav-link" href="#!">Home</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#!">Link</a></li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                            <div className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a className="dropdown-item" href="#!">Action</a>
+                                <a className="dropdown-item" href="#!">Another action</a>
+                                <div className="dropdown-divider"></div>
+                                <a className="dropdown-item" href="#!">Something else here</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+     )
 }
 
 export default Menubar;
