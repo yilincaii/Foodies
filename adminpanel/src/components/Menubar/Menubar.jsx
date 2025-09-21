8e82038a28b1f9d046f0c8a8747ab229c955a956
nayuanju <yic153@ucsd.edu>
2025-09-21 14:49:18 -0700

    feat(admin): hook up Menubar sidebar toggle (accept toggleSidebar prop, add onClick, replace button text with 'bi-list' icon)

--- adminpanel/src/components/Menubar/Menubar.jsx
+++ adminpanel/src/components/Menubar/Menubar.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const Menubar = () => {
+const Menubar = ({toggleSidebar}) => {
   return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
             <div className="container-fluid">
-                <button className="btn btn-primary" id="sidebarToggle">Toggle Menu</button>
+                <button className="btn btn-primary" id="sidebarToggle" onClick={toggleSidebar}> 
+                    <i className='bi bi-list'>
+
+                    </i>
+                </button>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
--- adminpanel/src/components/Sidebar/Sidebar.jsx
+++ adminpanel/src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Sidebar = () => {
+const Sidebar = ({sidebarVisible}) => {
     return (
-        <div className="border-end bg-white" id="sidebar-wrapper">
+        <div className={`border-end bg-white ${sidebarVisible ? '': 'd-none'}`} id="sidebar-wrapper">
             <div className="sidebar-heading border-bottom bg-light">Start Bootstrap</div>
             <div className="list-group list-group-flush">
                 <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/add">Add Food</Link>
