9f9dd6941afcf3819d7a3820750f5198bbc8cfe7
nayuanju <yic153@ucsd.edu>
2025-09-21 14:36:19 -0700

    fix(admin): import Link from react-router-dom in Sidebar to resolve runtime error

--- adminpanel/src/components/Sidebar/Sidebar.jsx
+++ adminpanel/src/components/Sidebar/Sidebar.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
-  return (
-    <div className="border-end bg-white" id="sidebar-wrapper">
-        <div className="sidebar-heading border-bottom bg-light">Start Bootstrap</div>
-        <div className="list-group list-group-flush">
-            <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Dashboard</a>
-            <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Shortcuts</a>
-            <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Overview</a>
-            <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Events</a>
-            <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Profile</a>
-            <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Status</a>
+    return (
+        <div className="border-end bg-white" id="sidebar-wrapper">
+            <div className="sidebar-heading border-bottom bg-light">Start Bootstrap</div>
+            <div className="list-group list-group-flush">
+                <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/add">Add Food</Link>
+                <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/list">List Food</Link>
+                <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/orders">Orders</Link>
+
+            </div> 
         </div> 
-    </div> 
-)
+    )
 }
 
 export default Sidebar;
