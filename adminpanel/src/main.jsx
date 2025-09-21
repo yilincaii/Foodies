f0bef18c119646fe98d6ee96aa209df7ed41759d
nayuanju <yic153@ucsd.edu>
2025-09-21 11:33:52 -0700

    style(admin): add Google Font (Poppins) + global reset in index.cs

--- adminpanel/src/main.jsx
+++ adminpanel/src/main.jsx
@@ -1,7 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
