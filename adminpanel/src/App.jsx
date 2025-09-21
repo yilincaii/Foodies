fd797a497665d3aa09a930457c07289decd76ae3
nayuanju <yic153@ucsd.edu>
2025-09-21 11:52:05 -0700

    initialize all the componets .jsx file

--- adminpanel/src/App.jsx
+++ adminpanel/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Routes } from 'react-router-dom';
 
 const App = () => {
   return (
@@ -37,15 +38,12 @@ const App = () => {
                     </div>
                 </nav>
                 <div className="container-fluid">
-                    <h1 className="mt-4">Simple Sidebar</h1>
-                    <p>The starting state of the menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will change.</p>
-                    <p>
-                        Make sure to keep all page content within the
-                        <code>#page-content-wrapper</code>
-                        . The top navbar is optional, and just for demonstration. Just create an element with the
-                        <code>#sidebarToggle</code>
-                        ID which will toggle the menu when clicked.
-                    </p>
+                    <Routes>
+                      <Route path='/add' element={<AddFood/>}/>
+                      <Route path='/list' element={<ListFood/>}/>
+                      <Route path='/orders' element={<Orders/>}/>
+                      <Route path='/' element={<ListFood/>}/>
+                    </Routes>
                 </div>
             </div>
         </div>
---
+++ adminpanel/src/pages/ListFood/ListFood.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ListFood = () => {
+  return (
+    <div>ListFood</div>
+  )
+}
+
+export default ListFood;
