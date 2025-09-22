823548355d1d9fea6846cafe72415560db5be166
nayuanju <yic153@ucsd.edu>
2025-09-22 10:12:52 -0700

    add logo at the left corner

--- adminpanel/src/components/Menubar/Menubar.jsx
+++ adminpanel/src/components/Menubar/Menubar.jsx
@@ -9,22 +9,6 @@ const Menubar = ({toggleSidebar}) => {
 
                     </i>
                 </button>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
-                        <li className="nav-item active"><a className="nav-link" href="#!">Home</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#!">Link</a></li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                            <div className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a className="dropdown-item" href="#!">Action</a>
-                                <a className="dropdown-item" href="#!">Another action</a>
-                                <div className="dropdown-divider"></div>
-                                <a className="dropdown-item" href="#!">Something else here</a>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
             </div>
         </nav>
      )
--- adminpanel/src/components/Sidebar/Sidebar.jsx
+++ adminpanel/src/components/Sidebar/Sidebar.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {assets} from '../../assets/assets';
 
 const Sidebar = ({sidebarVisible}) => {
     return (
-        <div className={`border-end bg-white ${sidebarVisible ? '': 'd-none'}`} id="sidebar-wrapper">
-            <div className="sidebar-heading border-bottom bg-light">Start Bootstrap</div>
-            <div className="list-group list-group-flush">
-                <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/add">Add Food</Link>
-                <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/list">List Food</Link>
-                <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/orders">Orders</Link>
-
-            </div> 
-        </div> 
-    )
+    <div className={`border-end bg-white ${sidebarVisible ? '': 'd-none'}`} id="sidebar-wrapper">
+        <div className="sidebar-heading border-bottom bg-light">
+            <img src={assets.logo} alt="" height={32} width={32}/>
+        </div>
+        <div className="list-group list-group-flush">
+            <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/add">
+            <i className='bi bi-plus-circle me-2'></i> Add Food</Link>
+            <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/list">
+            <i className='bi bi-list-ul me-2'></i> List Food</Link>
+            <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/orders">
+            <i className='bi bi-cart me-2'></i> Orders</Link>
+        </div>
+    </div>
+  )
 }
 
 export default Sidebar;
