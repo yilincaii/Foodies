d166affbba80c31f56a409dd7d7da146ab9cf45c
nayuanju <yic153@ucsd.edu>
2025-09-22 10:25:34 -0700

    add food form created

--- adminpanel/src/pages/AddFood/AddFood.jsx
+++ adminpanel/src/pages/AddFood/AddFood.jsx
@@ -2,8 +2,32 @@ import React from 'react';
 
 const AddFood = () => {
   return (
-    <div>AddFood</div>
-  )
+        <div className="container mt-2">
+      <div className="row">
+        <div className="card col-md-4">
+          <div className="card-body">
+            <h2 className="mb-4">Add Food</h2>
+            <form>
+              <div className="mb-3">
+                <label for="name" className="form-label">Your Name</label>
+                <input type="text" className="form-control" id="name" required />
+              </div>
+              <div className="mb-3">
+                <label for="email" className="form-label">Your Email</label>
+                <input type="email" className="form-control" id="email" required />
+              </div>
+              <div className="mb-3">
+                <label for="message" className="form-label">Your Message</label>
+                <textarea className="form-control" id="message" rows="5" required></textarea>
+              </div>
+              <button type="submit" className="btn btn-primary">Send Message</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+)
 }
 
 export default AddFood;
