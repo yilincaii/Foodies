6dc26017dc55a072c41fbcce8b9d047200bdcfa0
nayuanju <yic153@ucsd.edu>
2025-09-23 15:45:12 -0700

    feat: add scroll left/right button function to the home 'explore our menu'

--- foodies/src/components/ExploreMenu/ExploreMenu.jsx
+++ foodies/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,23 +1,39 @@
 import React from 'react';
 import { categories } from '../../assets/assets';
+import './ExploreMenu.css';
+import { use } from 'react';
+import { useRef } from 'react';
 
 const ExploreMenu = () => {
+
+    const menuRef = useRef(null);
+    const scrollLeft = () => {
+        if(menuRef.current){
+            menuRef.current.scrollBy({left: -200, behavior: 'smooth'});
+        }
+    };
+
+    const scrollRight = () => {
+        if(menuRef.current){
+            menuRef.current.scrollBy({left: 200, behavior: 'smooth'});
+        }
+    };    
   return (
     <div className="explore-menu position-relative">
         <h1 className="d-flex align-items-center justify-content-between">
             Explore Our Menu
             <div className="d-flex">
-            <i className='bi bi-arrow-left-circle scroll-icon'></i>
-            <i className='bi bi-arrow-right-circle scroll-icon'></i>
+            <i className='bi bi-arrow-left-circle scroll-icon' onClick={scrollLeft}></i>
+            <i className='bi bi-arrow-right-circle scroll-icon' onClick={scrollRight}></i>
         </div>
         </h1>
         <p>Explor curated lists of dishes from top categories</p>
-        <div className="d-flex justify-content-between gap-4 overflow-auto explore-menu-list">
+        <div className="d-flex justify-content-between gap-4 overflow-auto explore-menu-list" ref={menuRef}>
             {
                 categories.map((item, index) => {
                     return (
                         <div key={index} className="text-center explore-menu-list-item">
-                            <img src={item.icon} alt=""  className='rounded-circle'/>
+                            <img src={item.icon} alt=""  className='rounded-circle' height={128}width={128}/>
                             <p className='mt-2 fw-bold'>{item.category}</p>
                         </div>
                     )
