af0a9c991e6eb93b15f4138a0d41d6da2b999b7e
nayuanju <yic153@ucsd.edu>
2025-09-22 17:30:33 -0700

    (test) data tyeped in form can show on console correctly

--- adminpanel/src/pages/AddFood/AddFood.jsx
+++ adminpanel/src/pages/AddFood/AddFood.jsx
@@ -1,9 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {assets} from '../../assets/assets';
 
 const AddFood = () => {
 
   const [image, setImage] = useState(false);
+  const [data,setData] = useState({
+    name: '',
+    description: '',
+    price: '',
+    category: ''
+  });
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData(data => ({
+      ...data,
+      [name]: value
+    }));
+  }
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+  
   return (
         <div className="mx-2 mt-2">
       <div className="row">
@@ -19,16 +39,16 @@ const AddFood = () => {
               </div>
               <div className="mb-3">
                 <label htmlfor="name" className="form-label">Name</label>
-                <input type="text" className="form-control" id="name" required name='name' />
+                <input type="text" className="form-control" id="name" required name='name' onChange={onChangeHandler} value={data.name}/>
               </div>
             
               <div className="mb-3">
                 <label htmlfor="message" className="form-label">Description</label>
-                <textarea className="form-control" id="description" rows="5" required name ='description'></textarea>
+                <textarea className="form-control" id="description" rows="5" required name ='description' onChange={onChangeHandler} value={data.description}></textarea>
               </div>
               <div className="mb-3">
                 <label htmlfor="category" className="form-label">Category</label>
-                <select name="category" id="category" className='form-control' >
+                <select name="category" id="category" className='form-control' onChange={onChangeHandler} value={data.category} >
                   <option value="Biryani">Biryani</option>
                   <option value="Cake">Cake</option>
                   <option value="Burger">Buger</option>
@@ -40,7 +60,7 @@ const AddFood = () => {
               </div>
               <div className="mb-3">
                 <label htmlFor="price" className="form-label">Price</label>
-                <input type="number" name="price" id="price" className='form-control'  />
+                <input type="number" name="price" id="price" className='form-control'  onChange={onChangeHandler} value={data.price}/>
               </div>
               <button type="submit" className="btn btn-primary">Send Message</button>
             </form>
