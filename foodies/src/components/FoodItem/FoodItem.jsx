b45f1c5ff298f76c2cedd98b37739168bbf671b2
nayuanju <yic153@ucsd.edu>
2025-09-25 19:03:54 -0700

    feat: add increase/decrease quantity logic

--- foodies/src/components/FoodItem/FoodItem.jsx
+++ foodies/src/components/FoodItem/FoodItem.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import { Link } from "react-router-dom";
-//import { StoreContext } from "../../context/StoreContext";
+import { StoreContext } from '../../pages/Contact/StoreContext';
+
 const FoodItem = ({name, description,id,imageUrl,price}) => {
-  //const { increaseQty, decreaseQty, quantities } = useContext(StoreContext);
+
+  const { increaseQty, decreaseQty, quantities } = useContext(StoreContext);
 
   return (
     <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4 d-flex justify-content-center">
-      <Link to={`/food/${id}`}className="card" style={{ maxWidth: "320px","textDecoration" : "none" }}>
-        <img src={imageUrl}  className="card-img-top" alt="Product Image" height={300} width={60}/>
+      <div className="card" style={{ maxWidth: "320px"}}>
+      <Link to={`/food/${id}`}>
+        <img src={imageUrl}  className="card-img-top" alt="Product Image" height={300} width={60}/> 
+      </Link>
      
         <div className="card-body">
           <h5 className="card-title">{name}</h5>
@@ -26,9 +30,19 @@ const FoodItem = ({name, description,id,imageUrl,price}) => {
         </div>
         <div className="card-footer d-flex justify-content-between bg-light">
           <Link className="btn btn-success btn-sm" to={`/food/${id}`}>View Food</Link>
-          <button className="btn btn-outline-secondary btn-sm"><i className="bi bi-heart"></i></button>
+          {quantities[id] > 0 ? (
+            <div className="d-flex align-items-center gap-2">
+              <button className="btn btn-danger btn-sm" onClick={()=> decreaseQty(id)}><i className="bi bi-dash-circle"></i></button>
+              <span className="fw-bold">{quantities[id]}</span>
+              <button className="btn btn-success btn-sm" onClick={()=>increaseQty(id)}><i className="bi bi-plus-circle"></i></button>
+            </div>
+
+          ):(
+            <button className="btn btn-primary btn-sm" onClick={()=>increaseQty(id)}>
+              <i className="bi bi-plus-circle"></i></button>
+          )}
         </div>
-      </Link>
+      </div>
     </div>
   )
 }
--- foodies/src/pages/Contact/StoreContext.jsx
+++ foodies/src/pages/Contact/StoreContext.jsx
@@ -6,10 +6,24 @@ export const StoreContext = createContext(null);
 import { fetchFoodList } from "../../service/foodService";
 export const StoreContextProvider = (props) => {
     const [foodList, setFoodList] = useState([]);
+    const [quantities, setQuantities] = useState({
+        
+    });
+
+    const increaseQty=(foodId) => {
+
+        setQuantities((prev) => ({...prev,[foodId]: (prev[foodId] || 0) + 1 }));
+    }
+    const decreaseQty=(foodId) => {
+        setQuantities((prev) => ({...prev,[foodId]: prev[foodId] > 0 ? prev[foodId] -1 : 0 }));
+    }
 
 
     const contextValue = {
-        foodList
+        foodList,
+        increaseQty,
+        decreaseQty,
+        quantities
     };
 
     useEffect(() => {
