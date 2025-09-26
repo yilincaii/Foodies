09ac52c56b559758b2ea9b7fb429533f805562f3
nayuanju <yic153@ucsd.edu>
2025-09-25 17:15:09 -0700

    feat: after clicking the view food, guide to correct food detail page with iamge, price and discription

--- foodies/src/pages/FoodDetails/FoodDetails.jsx
+++ foodies/src/pages/FoodDetails/FoodDetails.jsx
@@ -2,40 +2,42 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { fetchFoodDetails } from '../../service/foodService';
+import { toast } from 'react-toastify';  
 
 
 const FoodDetails = () => {
     const {id}= useParams();
 
     const [data, setData] = useState({});
-    const fetchFoodDetails = async(id) => {
-        const response = await axios.get(`http://localhost:8081/api/foods/${id}`);
-        if(response.status === 200) {
-            setData(response.data);
-            console.log(response.data);
-        }
-        
-    }
+    
+
 
     useEffect(() => {
-        fetchFoodDetails(id);
+        const loadFoodDetails = async () => {
+            try {
+                const foodData = await fetchFoodDetails(id);
+                setData(foodData);
+            } catch (error) {
+                toast.error('Error displaying the food details:');
+            }
+        }
+        loadFoodDetails();
     }, [id]);
 
   return (
     <section className="py-5">
         <div className="container px-4 px-lg-5 my-5">
             <div className="row gx-4 gx-lg-5 align-items-center">
-                <div className="col-md-6"><img className="card-img-top mb-5 mb-md-0" src="https://dummyimage.com/600x700/dee2e6/6c757d.jpg" alt="..." /></div>
+                <div className="col-md-6"><img className="card-img-top mb-5 mb-md-0" src={data.imageUrl} alt="..." /></div>
                 <div className="col-md-6">
-                    <div className="small mb-1">SKU: BST-498</div>
-                    <h1 className="display-5 fw-bolder">Shop item template</h1>
+                    <div className="small mb-1">Category: <span className='badge text-bg-warning'>{data.category}</span></div>
+                    <h1 className="display-5 fw-bolder">{data.name}</h1>
                     <div className="fs-5 mb-5">
-                        <span className="text-decoration-line-through">$45.00</span>
-                        <span>$40.00</span>
+                        <span>&#36;{data.price}</span>
                     </div>
-                    <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?</p>
+                    <p className="lead">{data.description}</p>
                     <div className="d-flex">
-                        <input className="form-control text-center me-3" id="inputQuantity" type="num"  style={{"maxWidth": "3rem"}} />
                         <button className="btn btn-outline-dark flex-shrink-0" type="button">
                             <i className="bi-cart-fill me-1"></i>
                             Add to cart
--- foodies/src/service/foodService.js
+++ foodies/src/service/foodService.js
@@ -8,7 +8,19 @@ export const fetchFoodList = async () => {
         const response = await axios.get(API_URL);
         return response.data;
     } catch (error) {
-        console.log('Error', error);
+        console.log('Error fetching the food list', error);
+        throw error;
+        
+    }
+    
+}
+
+export const fetchFoodDetails = async(id) => {
+    try {
+        const response = await axios.get(`${API_URL}/${id}`);
+        return response.data;
+    } catch (error) {
+        console.log('Error fetching food details', error);
         throw error;
         
     }
